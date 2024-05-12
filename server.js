0f8d1b0ec44264f0001bf05bbd8ed13b48b8cfda
Megan Hilbert <meganmhilbert@gmail.com>
2024-05-12 18:07:18 -0500

    dynamic port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const blockPrivateRoutes = require('./middleware/blockPrivateRoutes')
 const routes = require('./routes')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 // global middleware
 
